5140ac50347413f6eb9efae7fc2a394c7ba6a21e
atomicpages <atomicpages@gmail.com>
2019-05-22 15:34:21 -0700

    Fixing #91
    * Adding tests for React.memo
    * Adding tests for React.forwardRef
    * Adding basic hook tests

--- lib/index.ts
+++ lib/index.ts
@@ -18,6 +18,57 @@ const setAlert = w => {
   return w
 }
 
+const cachedDisplayNames: WeakMap<JSX, string> = new WeakMap();
+
+/**
+ * Gets the display name of the component when possible.
+ * @param type {JSX} The type object returned from creating the react element.
+ * @param fallbackName {string} The alias, or fallback name to use when the name cannot be derived.
+ * @link https://github.com/facebook/react-devtools/blob/master/backend/getDisplayName.js
+ */
+function getDisplayName(type: JSX, fallbackName: string = 'Unknown'): string {
+  const nameFromCache = cachedDisplayNames.get(type)
+
+  if (nameFromCache != null) {
+    return nameFromCache
+  }
+
+  let displayName: string
+
+  // The displayName property is not guaranteed to be a string.
+  // It's only safe to use for our purposes if it's a string.
+  // github.com/facebook/react-devtools/issues/803
+  if (typeof type.displayName === 'string') {
+    displayName = type.displayName
+  }
+
+  if (!displayName) {
+    displayName = type.name || fallbackName
+  }
+
+  // Facebook-specific hack to turn "Image [from Image.react]" into just "Image".
+  // We need displayName with module name for error reports but it clutters the DevTools.
+  const match = displayName.match(/^(.*) \[from (.*)\]$/)
+
+  if (match) {
+    const componentName = match[1]
+    const moduleName = match[2]
+
+    if (componentName && moduleName) {
+      if (
+        moduleName === componentName ||
+        moduleName.startsWith(componentName + '.')
+      ) {
+        displayName = componentName
+      }
+    }
+  }
+
+  cachedDisplayNames.set(type, displayName)
+
+  return displayName
+}
+
 /** Initialize an empty document w/ ReactDOM and DOM events.
     @function   cy.injectReactDOM
 **/
@@ -106,7 +157,7 @@ Cypress.Commands.add('copyComponentStyles', component => {
  **/
 export const mount = (jsx, alias) => {
   // Get the display name property via the component constructor
-  const displayname = alias || jsx.type.prototype.constructor.name
+  const displayname = getDisplayName(jsx.type, alias)
 
   let cmd
 
@@ -165,7 +216,8 @@ Cypress.Commands.overwrite('get', (originalFn, selector, options) => {
       }
     case 'function':
       // If attempting to use the component name without JSX (testing in .js/.ts files)
-      const displayname = selector.prototype.constructor.name
+      // const displayname = selector.prototype.constructor.name
+      const displayname = getDisplayName(selector.type);
       return originalFn(`@${displayname}`, options)
     default:
       return originalFn(selector, options)
