ac5c53f6ae4239a82e65559f4d061dec7a2b6bb8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-05-13 14:02:14 -0400

    example test

--- examples/visual-testing-with-happo/src/Calendar.cy-spec.js
+++ examples/visual-testing-with-happo/src/Calendar.cy-spec.js
@@ -6,5 +6,21 @@ import Calendar from './Calendar'
 describe('Calendar heatmap', () => {
   it('random data', () => {
     mount(<Calendar />)
+    cy.get('.react-calendar-heatmap').happoScreenshot({
+      component: 'Calendar',
+      variant: 'random',
+    })
+
+    cy.log('Check tooltip')
+    // first there should be not toolip
+    cy.get('[data-id=tooltip]').should('not.be.visible')
+
+    // select a day using part of the attribute "data-tip"
+    // full attribute: data-tip="2019-12-29 has count: 4"
+    cy.get('[data-tip^="2019-12-29"]').click()
+    cy.get('[data-id=tooltip]').happoScreenshot({
+      component: 'Tooltip',
+      variant: 'random',
+    })
   })
 })
