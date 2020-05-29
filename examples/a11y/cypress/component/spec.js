4f4cc80ed27af506d3cc24f655271604946fff3f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-05-29 09:33:00 -0400

    add tests

--- examples/a11y/cypress/component/spec.js
+++ examples/a11y/cypress/component/spec.js
@@ -3,18 +3,40 @@ import React from 'react'
 import { mount } from 'cypress-react-unit-test'
 
 describe('Accessibility', () => {
+  before(() => {
+    // https://github.com/avanslaars/cypress-axe
+    cy.injectAxe()
+  })
+
+  // https://www.w3.org/WAI/standards-guidelines/aria/
   context('aria', () => {
+    // skip a failing test on purpose
+    it.skip('catches missing aria-* label', () => {
+      mount(<input type="text" value="John Smith" name="name" />)
+      cy.checkA11y('input', {
+        runOnly: {
+          type: 'tag',
+          values: ['wcag2a'],
+        },
+      })
+    })
+
     it('passes', () => {
       mount(
         <input
           type="text"
-          aria-label={labelText}
+          aria-label="label text"
           aria-required="true"
-          onChange={onchangeHandler}
-          value={inputValue}
+          value="John Smith"
           name="name"
         />,
       )
+      cy.checkA11y('input', {
+        runOnly: {
+          type: 'tag',
+          values: ['wcag2a'],
+        },
+      })
     })
   })
 })
