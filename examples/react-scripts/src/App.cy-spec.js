73364ebfd22c1ea20443b7393343dabbfd56c3d6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-05-30 12:44:50 -0400

    set loose modules in react-scripts example

--- examples/react-scripts/src/App.cy-spec.js
+++ examples/react-scripts/src/App.cy-spec.js
@@ -3,10 +3,19 @@
 import React from 'react'
 import App from './App'
 import { mount } from 'cypress-react-unit-test'
+import * as calc from './calc'
 
 describe('App', () => {
   it('renders learn react link', () => {
     mount(<App />)
     cy.contains(/Learn React/)
+  })
+
+  it('controls the random number by stubbing named import', () => {
+    // we are stubbing "getRandomNumber" exported by "calc.js"
+    // and imported into "App.js" and called.
+    cy.stub(calc, 'getRandomNumber').returns(777)
+    mount(<App />)
+    cy.contains('.random', '777')
   })
 })
