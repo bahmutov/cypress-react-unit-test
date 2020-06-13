1348dfbf2434d51b5907b18632e43758ad113ad4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-06-13 08:33:26 -0400

    load fixtures correctly

--- examples/react-scripts/src/fixture.cy-spec.js
+++ examples/react-scripts/src/fixture.cy-spec.js
@@ -5,11 +5,12 @@ import names from './names.json'
 // try importing JavaScript
 import { add } from '../cypress/fixtures/add'
 // try importing JSON from fixtures folder
-// import fixtureNames from '../cypress/fixtures/names.json'
+import fixtureNames from '../cypress/fixtures/names.json'
 
 describe('fixtures', () => {
   it('imports the array', () => {
     expect(names).to.deep.equal(['joe', 'mary'])
-    // expect(fixtureNames).to.deep.equal(['joe', 'mary'])
+    expect(fixtureNames).to.deep.equal(['joe', 'mary'])
+    expect(add(2, 4)).to.equal(6)
   })
 })
