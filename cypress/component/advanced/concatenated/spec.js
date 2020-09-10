f67e1d0eb00121ad600475d40fef865fcd068aa7
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2020-09-10 11:00:02 +0300

    Fix concatenated

--- cypress/component/advanced/concatenated/spec.js
+++ cypress/component/advanced/concatenated/spec.js
@@ -1,12 +1,11 @@
-;(function() {
-  import React from 'react'
-  import { mount } from 'cypress-react-unit-test'
+import React from 'react'
+import { mount } from 'cypress-react-unit-test'
 
-  describe('Concatenated 1', () => {
-    const Hello = () => <div>First</div>
-    it('renders', () => {
-      mount(<Hello />)
-      cy.contains('First').should('be.visible')
-    })
+describe('Concatenated 1', () => {
+  const Hello = () => <div>First</div>
+
+  it('renders', () => {
+    mount(<Hello />)
+    cy.contains('First').should('be.visible')
   })
-})()
+})
