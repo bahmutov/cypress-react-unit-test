f213cc6267f42379578827e970344eaa55cdb7bf
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2020-09-11 19:50:21 +0300

    Add css modules example

---
+++ cypress/component/basic/css-modules/Button.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import * as PropTypes from 'prop-types'
+import styles from './Button.modules.css'
+
+console.log(styles)
+
+export const Button = ({ name, orange, wide }) => {
+  const className = [
+    styles.componentButton,
+    orange ? styles.orange : '',
+    wide ? styles.wide : '',
+  ]
+
+  return (
+    <div className={className.join(' ').trim()}>
+      <button>{name}</button>
+    </div>
+  )
+}
+
+Button.propTypes = {
+  orange: PropTypes.bool,
+  wide: PropTypes.bool,
+  name: PropTypes.string.isRequired,
+}
