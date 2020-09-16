07ea4760b273d748904e683497175deba9bcbf32
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-09-16 13:19:03 -0400

    expand in-memory spec

--- cypress/component/advanced/react-router-v6/in-memory-spec.js
+++ cypress/component/advanced/react-router-v6/in-memory-spec.js
@@ -4,29 +4,40 @@ import { mount } from 'cypress-react-unit-test'
 import { App } from './app.jsx'
 import { MemoryRouter } from 'react-router-dom'
 
-it('navigates through the link without changing url', () => {
-  cy.viewport(600, 300)
-  mount(
-    <MemoryRouter
-      initialEntries={['/about', '/two', { pathname: '/three' }]}
-      initialIndex={0}
-    >
-      <App />
-    </MemoryRouter>,
-  )
+describe('React Memory Router', () => {
+  it('navigates through the link without changing url', () => {
+    cy.viewport(600, 300)
+    mount(
+      <MemoryRouter
+        initialEntries={['/about', '/two', { pathname: '/three' }]}
+        initialIndex={0}
+      >
+        <App />
+      </MemoryRouter>,
+    )
 
-  // we are mocking the initial open route with `initialIndex`
-  cy.contains('h2', 'About')
+    // because the routing is in memory, the URL stays at the spec filename
+    cy.location('pathname').should('match', /in-memory-spec.js$/)
 
-  // Go to home route
-  cy.contains('a', 'Home').click()
+    // we are mocking the initial open route with `initialIndex`
+    // so we should see "About" component
+    cy.log('**About** component')
+    cy.contains('h2', 'About')
 
-  cy.contains('h2', 'Home')
-  cy.location('pathname').should('not.equal', '/')
+    // Go to home route
+    cy.contains('a', 'Home').click()
 
-  // Go to about route
-  cy.contains('a', 'About').click()
+    cy.log('**Home** component')
+    cy.contains('h2', 'Home') // from the "Home" component
+    // still at the spec url
+    cy.location('pathname').should('match', /in-memory-spec.js$/)
 
-  cy.contains('h2', 'About')
-  cy.location('pathname').should('not.equal', '/about')
+    // Go to about route
+    cy.log('back to **About** component')
+    cy.contains('a', 'About').click()
+
+    cy.contains('h2', 'About')
+    // still at the spec url
+    cy.location('pathname').should('match', /in-memory-spec.js$/)
+  })
 })
