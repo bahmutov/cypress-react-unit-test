551a5fe64db05d762fb1c068321798e5749a75b4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-10-07 09:50:29 -0400

    refactor basic typescript example

---
+++ cypress/component/basic/typescript/README.md
@@ -0,0 +1,5 @@
+# typescript
+
+You can write your components and specs using TypeScript if your preprocessor can transpile `.tsx` files.
+
+- [ts-spec.tsx](ts-spec.tsx) file exercises [Button.tsx](Button.tsx) component
--- cypress/component/basic/typescript/ts-spec.tsx
+++ cypress/component/basic/typescript/ts-spec.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { mount } from 'cypress-react-unit-test'
-
-const Button = ({ children, ...rest }) => {
-  return <button {...rest}>{children}</button>
-}
+import { Button } from './Button'
 
 describe('Component spec in typescript', () => {
   it('works', () => {
     mount(<Button>Button Label</Button>)
     cy.contains('button', 'Button Label').should('be.visible')
+  })
+
+  it('calls passed on click prop', () => {
+    mount(<Button onClick={cy.stub().as('click')}>Click Me</Button>)
+    cy.contains('button', 'Click Me').click()
+    cy.get('@click').should('have.been.calledOnce')
   })
 })
