0f9b3f7d51260b679c954d5fdc49c1b77f0a4deb
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-10-07 09:57:46 -0400

    example with TypeScript path alias

--- cypress/component/basic/typescript/ts-spec.tsx
+++ cypress/component/basic/typescript/ts-spec.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { mount } from 'cypress-react-unit-test'
 import { Button } from './Button'
+// import the same Button component using path alias
+// that should be set up in webpack AND in tsconfig
+// https://glebbahmutov.com/blog/using-ts-aliases-in-cypress-tests/
+import { Button as SameButton } from '@basic/typescript/Button'
 
 describe('Component spec in typescript', () => {
   it('works', () => {
@@ -12,5 +16,15 @@ describe('Component spec in typescript', () => {
     mount(<Button onClick={cy.stub().as('click')}>Click Me</Button>)
     cy.contains('button', 'Click Me').click()
     cy.get('@click').should('have.been.calledOnce')
+  })
+
+  it('loads the component using path alias', () => {
+    mount(
+      <SameButton onClick={cy.stub().as('click')}>Click Me Twice</SameButton>,
+    )
+    cy.contains('button', 'Click Me Twice')
+      .click()
+      .click()
+    cy.get('@click').should('have.been.calledTwice')
   })
 })
