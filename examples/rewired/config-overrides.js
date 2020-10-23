befd23bcb0942bd80976106fd16545c64ccc4eed
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-10-23 09:37:13 -0400

    working

--- examples/rewired/config-overrides.js
+++ examples/rewired/config-overrides.js
@@ -1,4 +1,6 @@
-const debug = require('debug')('cypress-react-unit-test')
+// to see these debug messages, run the program with
+// DEBUG=overrides
+const debug = require('debug')('overrides')
 
 module.exports = function override(config, env) {
   debug('in config overrides, env "%s"', env)
@@ -7,7 +9,7 @@ module.exports = function override(config, env) {
   // throw an error to stop the app
   // useful for debugging to avoid the dev server
   // from hiding the console output
-  throw new Error('stop here')
+  // throw new Error('stop here')
 
   //do stuff with the webpack config...
 
